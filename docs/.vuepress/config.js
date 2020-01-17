68c5e3c22a2b41a9aaa571632c9afb0751bf3a58
billyqin <965824981@qq.com>
2020-01-17 10:33:12 +0800

    checkout to vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "BillyQin's Blog",
+  description: 'This is BillyQin Blog',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '关于', link: '/guide/' },
+      { text: 'Github', link: 'https://github.com/BillyQin/BillyQin.github.io' },
+    ],
+
+  }
+}
